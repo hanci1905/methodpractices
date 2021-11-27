6bc37e4b2c292c941f383a5fd36ca5e0ceaccd0e
hanci1905 <kaanfre@gmail.com>
2021-11-28 02:35:20 +0300

    Palindrome

---
+++ Prime.java
@@ -0,0 +1,31 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class AdvancedCalculator {
+    static boolean isPrime(int n,int i) {
+        // Base cases
+        if (n <= 2)
+            return (n == 2) ? true : false;
+        if (n % i == 0)
+            return false;
+        if (i * i > n)
+            return true;
+
+        // Check for next divisor
+        return isPrime(n, i + 1);
+
+    }
+
+    public static void main(String[] args) {
+    System.out.println("enter num");
+    Scanner input=new Scanner(System.in);
+    int n= input.nextInt();
+
+        if (isPrime(n, 2))
+            System.out.println(n + " sayisi asaldir");
+        else
+            System.out.println(n + "sayisi asal degildir");
+
+    }
+}
